e598f36a2cb44cce1ffa5b73ff7ffacfbe5938be
Cameron Wong <cwdev4772@gmail.com>
2025-03-30 20:14:52 -0400

    Updates

--- sw.js
+++ sw.js
@@ -33,11 +33,11 @@ self.__precacheManifest = [
     "url": "framework-8b85362f4e604ef6475f.js"
   },
   {
-    "url": "app-71800970c869395b3753.js"
+    "url": "app-ee3f4fa760b2534e9c6d.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "02057f58123b443495ed3748eb51d43b"
+    "revision": "3cfc77c68af35a8d849f2340b467823b"
   },
   {
     "url": "manifest.webmanifest",
@@ -133,7 +133,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-71800970c869395b3753.js`))) {
+  if (!resources || !(await caches.match(`/app-ee3f4fa760b2534e9c6d.js`))) {
     return await fetch(event.request)
   }
 
